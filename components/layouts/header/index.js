8bc9373c0811db4f2b6ceb41e185a3e89adc5860
sachin kumar gautam <sachinbvp100@gmailcom>
2018-05-29 13:46:49 +0530

    updating

---
+++ components/layouts/header/index.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { withStyles } from '@material-ui/core/styles'
+import AppBar from '@material-ui/core/AppBar'
+import Button from '@material-ui/core/Button'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import AutosuggestSearch from '../../AutosuggestSearch'
+
+const styles = {
+  root: {
+    flexGrow: 1
+  }
+}
+
+function SimpleAppBar (props) {
+  const { classes } = props
+  return (
+    <div className={classes.root}>
+      <AppBar position='static' color='default'>
+        <Toolbar>
+          <Typography variant='title' color='inherit'>
+            Title
+          </Typography>
+          <AutosuggestSearch />
+          <Button color='inherit'>Login</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  )
+}
+
+SimpleAppBar.propTypes = {
+  classes: PropTypes.object.isRequired
+}
+
+export default withStyles(styles)(SimpleAppBar)
