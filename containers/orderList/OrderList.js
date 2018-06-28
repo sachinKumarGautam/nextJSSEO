9f42ceb4ce04cd2b2fb35db030df1daa9e8ac9d2
Ayushi Khetan <ayushi.khetan@lifcare.in>
2018-06-28 09:07:55 +0530

    WIP

---
+++ containers/orderList/OrderList.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+/*
+  bread crumbs
+  order list
+*/
+
+class OrderList extends Component {
+  render () {
+    return (
+      <div>
+        hi
+      </div>
+    )
+  }
+}
+
+export default OrderList
