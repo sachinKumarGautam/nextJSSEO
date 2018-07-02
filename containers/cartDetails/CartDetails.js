6dce99fc567b8d78f30087d312e4c4c4277dd58c
jyotiarora2610 <jyotiarora2610@gmail.com>
2018-07-02 11:29:49 +0530

    WIP: cart page design

--- containers/cartDetails/CartDetails.js
+++ containers/cartDetails/CartDetails.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
 
+/*
+  avatar
+  medicine list
+  coupon
+  care points
+  care points plus
+  price details
+*/
+
 class CartDetails extends Component {
   render () {
     return (
       <div>
-      
+
       </div>
     )
   }
