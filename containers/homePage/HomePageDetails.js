f33befb4ad7abaf124cefed6d25fe874f23ab52d
Ayushi Khetan <ayushi.khetan@lifcare.in>
2018-07-11 15:54:33 +0530

    Home page

--- containers/homePage/HomePageDetails.js
+++ containers/homePage/HomePageDetails.js
@@ -3,15 +3,10 @@ import React, {Component} from 'react'
 import Slider from "react-slick";
 import { withStyles } from '@material-ui/core/styles'
 
-// import '../../pages/index.css'
-
 const styles = theme => {
   return {
-    container: {
-      margin: '0 auto',
-      padding: '35px 0px',
-      color: '#333',
-      background: '##80c241',
+    imageStyle: {
+      width: '100%'
     }
   }
 }
@@ -22,26 +17,24 @@ class HomePageWrapper extends Component {
       dots: false,
       arrows: false,
       infinite: true,
-      speed: 300,
+      speed: 1000,
       slidesToShow: 1,
       slidesToScroll: 1,
       autoplay: true,
       autoplaySpeed: 2000
     }
     return (
-      <div className={this.props.classes.container}>
-        <Slider {...settings}>
-          {
-            this.props.homePageState.backGroundImage.payload.map((item) => {
-              return (
-                <div>
-                  <img src={item.image}/>
-                </div>
-              )
-            })
-          }
-        </Slider>
-      </div>
+      <Slider {...settings}>
+        {
+          this.props.homePageState.backGroundImage.payload.map((item) => {
+            return (
+              <div>
+                <img src={item.image} className={this.props.classes.imageStyle}/>
+              </div>
+            )
+          })
+        }
+      </Slider>
     )
   }
 }
