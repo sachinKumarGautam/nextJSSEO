77ca097bec9e0dcc333ed8334644375e122f151f
jyotiarora2610 <jyotiarora2610@gmail.com>
2018-07-16 15:36:49 +0530

    popover bug fix

--- components/ProductMolecule.js
+++ components/ProductMolecule.js
@@ -71,31 +71,34 @@ class ProductMolecule extends Component {
     return (
       <div className={classes.horizontalItem}>
         {
-          this.props.salts && this.props.salts.length && this.props.salts.map((item) => (
-            <div>
-              <a
-                href='#'
-                className={classes.moleculeTag}
-                // onClick={this.handlePopoverOpen}
-                data-tip
-                data-for='moleculeItem'
-                // onMouseOver={this.handlePopoverOpen}
-                // onMouseOut={this.handlePopoverClose}
-              >
-                {item.name}
-              </a>
-              <ReactTooltip
-                id='moleculeItem'
-                effect='solid'
-                place='right'
-                className={classes.paper}
-                delayHide={1000}
-                delayShow={1000}
-              >
-                <PopoverContent item={item} styles={classes} />
-              </ReactTooltip>
-            </div>
-          )
+          this.props.salts && this.props.salts.length && this.props.salts.map((item, index) => {
+            let popperId = 'moleculeItem' + index
+            return (
+              <div>
+                <a
+                  href='#'
+                  className={classes.moleculeTag}
+                  // onClick={this.handlePopoverOpen}
+                  data-tip
+                  data-for={popperId}
+                  // onMouseOver={this.handlePopoverOpen}
+                  // onMouseOut={this.handlePopoverClose}
+                >
+                  {item.name}
+                </a>
+                <ReactTooltip
+                  id={popperId}
+                  effect='solid'
+                  place='right'
+                  className={classes.paper}
+                  delayHide={1000}
+                  delayShow={1000}
+                >
+                  <PopoverContent item={item} styles={classes} />
+                </ReactTooltip>
+              </div>
+            )
+          }
           )
         }
       </div>
