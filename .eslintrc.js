d640d717b8f99a50ed05726dcfa21b1dd5a36c46
sachin kumar gautam <sachinbvp100@gmailcom>
2018-07-20 18:29:02 +0530

    eslint changes

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "extends": "standard",
+    // "parser": "babel-eslint",
+    "plugins": [
+        "react",
+        "jsx-ally",
+        "import"
+    ]
+};
--- components/HOCWrapper/CommonWrapper.js
+++ components/HOCWrapper/CommonWrapper.js
@@ -1,90 +1,97 @@
-import AddToCartWrapper from '../../containers/cartDetails/addToCartWrapper'
-import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
-import PincodeDialog from '../../containers/location/pincode/PincodeDialog'
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import PincodeDialog from '../../containers/location/pincode/PincodeDialog';
 
-import { incrementCartItemLoading } from '../../containers/cartDetails/cartActions'
-import { openPincodeDialog, checkPincodeLoading } from '../../containers/location/pincode/pincodeAction'
+import { incrementCartItemLoading } from '../../containers/cartDetails/cartActions';
+import {
+  openPincodeDialog,
+  checkPincodeLoading,
+} from '../../containers/location/pincode/pincodeAction';
 
-import withRoot from '../../src/withRoot'
+import withRoot from '../../src/withRoot';
 
-export function commonWrapperHOC (Page) {
+export function commonWrapperHOC(Page) {
   class CommomWrapper extends React.Component {
-    static getInitialProps (ctx) {
-      if (Page.getInitialProps) { return Page.getInitialProps(ctx) }
-    }
-
-    constructor (props) {
-      super(props)
-      this.addToCartHandler = this.addToCartHandler.bind(this)
-      this.state = {
-        inProgressCartItem: {}
+    static getInitialProps(ctx) {
+      if (Page.getInitialProps) {
+        return Page.getInitialProps(ctx);
       }
     }
 
-    addToCartHandler (inProgressCartItem, event) {
+    constructor(props) {
+      super(props);
+      this.addToCartHandler = this.addToCartHandler.bind(this);
+      this.state = {
+        inProgressCartItem: {},
+      };
+    }
+
+    addToCartHandler(inProgressCartItem, event) {
       if (this.props.checkPincodeState.payload.pincode) {
-        this.props.actions.incrementCartItemLoading(this.props.cartState, inProgressCartItem)
+        this.props.actions.incrementCartItemLoading(
+          this.props.cartState,
+          inProgressCartItem
+        );
       } else {
         this.setState({
-          inProgressCartItem
-        })
+          inProgressCartItem,
+        });
 
-        this.props.actions.openPincodeDialog(this.props.checkPincodeState, true)
+        this.props.actions.openPincodeDialog(
+          this.props.checkPincodeState,
+          true
+        );
       }
     }
 
-  handleClose = () => (
-    this.props.actions.openPincodeDialog(this.props.checkPincodeState, false)
-  )
+    handleClose = () =>
+      this.props.actions.openPincodeDialog(this.props.checkPincodeState, false);
 
-  render () {
-    const { checkPincodeState, actions } = this.props
-    const { inProgressCartItem } = this.state
-    return (
-      <React.Fragment>
-        <Page {...this.props}
-          addToCartHandler={this.addToCartHandler}
-        />
-        <PincodeDialog
-          {...this.props}
-          open={checkPincodeState.isPincodeDialogOpen}
-          onSubmit={actions.checkPincodeLoading}
-          incrementCartItemLoading={actions.incrementCartItemLoading}
-          inProgressCartItem={inProgressCartItem}
-          handleClose={this.handleClose}
-          checkPincodeState={checkPincodeState}
-        />
-      </React.Fragment>
-    )
-  }
+    render() {
+      const { checkPincodeState, actions } = this.props;
+      const { inProgressCartItem } = this.state;
+      return (
+        <React.Fragment>
+          <Page {...this.props} addToCartHandler={this.addToCartHandler} />
+          <PincodeDialog
+            {...this.props}
+            open={checkPincodeState.isPincodeDialogOpen}
+            onSubmit={actions.checkPincodeLoading}
+            incrementCartItemLoading={actions.incrementCartItemLoading}
+            inProgressCartItem={inProgressCartItem}
+            handleClose={this.handleClose}
+            checkPincodeState={checkPincodeState}
+          />
+        </React.Fragment>
+      );
+    }
   }
 
-  function mapStateToProps (state) {
+  function mapStateToProps(state) {
     return {
       checkPincodeState: state.checkPincodeState,
       searchMedicineState: state.searchMedicineState,
-      cartState: state.cartState
-    }
+      cartState: state.cartState,
+    };
   }
 
-  function mapDispatchToProps (dispatch) {
+  function mapDispatchToProps(dispatch) {
     return {
       actions: bindActionCreators(
         {
           incrementCartItemLoading,
           openPincodeDialog,
-          checkPincodeLoading
+          checkPincodeLoading,
         },
         dispatch
-      )
-    }
+      ),
+    };
   }
 
   return connect(
     mapStateToProps,
     mapDispatchToProps
-  )(withRoot(CommomWrapper))
+  )(withRoot(CommomWrapper));
 }
 
 // export default (mapStateToProps, mapDispatchToProps) => (Page) => {
