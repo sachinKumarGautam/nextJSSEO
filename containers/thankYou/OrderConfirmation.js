7c008d66551728d4bae52dc4396b8f1413d274a1
sachin kumar gautam <sachinbvp100@gmailcom>
2018-08-08 16:47:23 +0530

    loader added on thank you page

--- containers/thankYou/OrderConfirmation.js
+++ containers/thankYou/OrderConfirmation.js
@@ -4,24 +4,17 @@ import Grid from '@material-ui/core/Grid'
 
 import OrderConfirmationDetails from './OrderConfirmationDetails'
 import MyCartDetails from './MyCartDetails'
-import ActivityIndicator from '../../components/activityIndicator/index'
-import CommonContentLoader
-  from '../../components/activityIndicator/loader/CommonContentLoader'
+
 
 const OrderConfirmation = props => {
   return (
     <Grid container>
       <Grid item xs={7}>
-        <ActivityIndicator
-          isLoading={props.thankYouState.isLoading}
-          LoaderComp={<CommonContentLoader />}
-        >
           <OrderConfirmationDetails
             cartState={props.cartState}
             submitRefillDateLoading={props.submitRefillDateLoading}
             thankYouState={props.thankYouState}
           />
-        </ActivityIndicator>
       </Grid>
       <Grid item xs={5}>
         <MyCartDetails cartState={props.cartState} />
