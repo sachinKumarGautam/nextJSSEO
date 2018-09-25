6f9d63f59880c4861af9bc24ff10e2ee0d59d8de
Rahul Jindal <rahul.jindal@lifcare.in>
2018-09-25 17:53:38 +0530

    fix medicine name

--- containers/searchMedicine/index.js
+++ containers/searchMedicine/index.js
@@ -296,7 +296,7 @@ class SearchMedicine extends React.Component {
                   fullWidth: true,
                   classes,
                   InputProps: getInputProps({
-                    placeholder: 'Search medicine...',
+                    placeholder: 'Search Medicines',
                     id: 'search-medicine',
                     autoFocus: true,
                     onChange: this.searchMedicineOnChange,
