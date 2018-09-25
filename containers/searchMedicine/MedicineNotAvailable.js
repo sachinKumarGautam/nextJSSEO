fe9ed762111e00bda9331c704888c0a62dee4fa1
sachin kumar gautam <sachinbvp100@gmailcom>
2018-09-25 14:30:28 +0530

    custom medicine

--- containers/searchMedicine/MedicineNotAvailable.js
+++ containers/searchMedicine/MedicineNotAvailable.js
@@ -4,26 +4,29 @@ import { withStyles } from '@material-ui/core/styles'
 
 const styles = theme => ({
   paper: {
-    position: 'absolute',
-    maxHeight: theme.spacing.unit * 50,
-    overflow: 'scroll',
+    height: theme.spacing.unit * 5,
     zIndex: 1,
     left: 0,
     right: 0,
     marginLeft: theme.spacing.unit * 4,
     marginRight: theme.spacing.unit * 2,
-    width: 'auto'
+    width: 'auto',
+    boxShadow: 'none'
+  },
+  text: {
+    marginTop: theme.spacing.unit * 2,
+    marginBottom: theme.spacing.unit * 2
   }
 })
 
 const MedicineNotAvailable = ({ classes }) => {
   return (
     <Paper className={classes.paper}>
-      <Typography>
+      <Typography className={classes.text}>
         This Medicine is not available with us currently.
       </Typography>
     </Paper>
   )
 }
 
-export default withStyles()(MedicineNotAvailable)
+export default withStyles(styles)(MedicineNotAvailable)
