e3a229fa7e478443a9510d3b46181df0eb5045ca
Rahul Jindal <rahul.jindal@lifcare.in>
2018-10-10 12:42:29 +0530

    change production keys

--- services/fetchUrl.js
+++ services/fetchUrl.js
@@ -11,8 +11,8 @@ import {
 import getApiServiceVersion from './apiVersion'
 
 // const baseUrl = process.env.REACT_APP_BASE_URL
-const baseUrl = 'http://sandbox.lifcare.in'
-// const baseUrl = 'https://lifcare.in'
+//const baseUrl = 'http://sandbox.lifcare.in'
+ const baseUrl = 'https://lifcare.in'
 
 function fetchUrl (
   apiServiceType,
