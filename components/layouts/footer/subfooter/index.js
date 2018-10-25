50b7c533a958062e8f0fbad222fc2d5112fce273
Rahul Jindal <rahul.jindal@lifcare.in>
2018-10-25 16:47:22 +0530

    desktop footer content

--- components/layouts/footer/subfooter/index.js
+++ components/layouts/footer/subfooter/index.js
@@ -18,6 +18,17 @@ const styles = theme => ({
     flexDirection: 'row',
     justifyContent: 'space-between',
     width: '100%'
+  },
+  footerContent : {
+    color : "#9b9b9b"
+  },
+  footerHeading : {
+    fontWeight : "bold",
+    fontSize : "16px",
+    color : '#fff'
+  },
+  aboveFooter : {
+    fontFamily : "Lato, sans-serif"
   }
 })
 
@@ -28,6 +39,15 @@ const SubFooter = ({classes}) => (
     component='div'
     square
   >
+    <div className = {classes.aboveFooter}>
+      <p className = {classes.footerHeading}>Welcome to LifCare - your one-stop solution for all chronic disease management needs!</p>
+      <div className={classes.footerContent}>
+        <p>LifCare is a subscription-based online pharmacy for chronic patients suffering from diabetes, hypertension, asthma, arthritis, gastritis, kidney, cardiac & neuro illnesses etc.</p>
+        <p>All LifCare members get exclusive benefits like FLAT discounts on every Online medicine order for the lifetime along with privileges like free doctor teleconsultation, diet counselling, and diagnostic lab tests for health checkups etc.</p>
+        <p>The official online medicine shopping app by LifCare allows additional benefits like easy one-click medicine re-fills, monthly reminders and additional discounts applicable only on the mobile app.</p>
+      </div>
+    </div>
+    <br/>
     <div className={classes.footerInnerWrapper}>
       <CompanyInfo />
       <ContactUs />
